ac36ed802402e9c478f8b5b1ef78bbb7da66bf27
maulasyakur <maulaahmad18@gmail.com>
2026-01-08 16:51:46 +0300

    initialized tiptap

--- src/pages/admin/BlogPostEditor.tsx
+++ src/pages/admin/BlogPostEditor.tsx
@@ -1,6 +1,12 @@
 import { Spinner } from "@/components/ui/spinner";
 import { usePost } from "@/lib/blog";
 import { useParams } from "react-router";
+import { useEditor, EditorContent, EditorContext } from "@tiptap/react";
+import { FloatingMenu, BubbleMenu } from "@tiptap/react/menus";
+import StarterKit from "@tiptap/starter-kit";
+import { useEffect, useMemo } from "react";
+import { Markdown } from "@tiptap/markdown";
+import { HeadingButton } from "@/components/tiptap-ui/heading-button";
 
 // TODO: Look for editor components that can:
 // - support markdown editing with preview
@@ -12,6 +18,27 @@ import { useParams } from "react-router";
 export default function BlogPostEditor() {
   const { slug } = useParams();
   const { data, isLoading, error } = usePost(slug!);
+  const editor = useEditor({
+    extensions: [StarterKit, Markdown], // define your extension array
+    content: "<p>Hello World!</p>", // initial content
+    editorProps: {
+      attributes: {
+        class:
+          "prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl m-5 focus:outline-none mx-auto",
+      },
+    },
+    contentType: "markdown",
+  });
+
+  useEffect(() => {
+    if (data && editor) {
+      editor.commands.setContent(data.content || "Hello World!", {
+        contentType: "markdown",
+      });
+    }
+  }, [data, editor]);
+
+  const providerValue = useMemo(() => ({ editor }), [editor]);
 
   if (isLoading) {
     return (
@@ -30,9 +57,13 @@ export default function BlogPostEditor() {
   }
 
   return (
-    <>
-      <div>BlogPostEditor, {slug}</div>
-      <div>{data?.content}</div>
-    </>
+    <EditorContext.Provider value={providerValue}>
+      <HeadingButton level={1}>Heading 1</HeadingButton>
+      <HeadingButton level={2}>Heading 2</HeadingButton>
+      <HeadingButton level={3}>Heading 3</HeadingButton>
+      <EditorContent editor={editor} />
+      <FloatingMenu editor={editor}>This is the floating menu</FloatingMenu>
+      <BubbleMenu editor={editor}>This is the bubble menu</BubbleMenu>
+    </EditorContext.Provider>
   );
 }
